45747c61dbdf9a1a0d62ad30f82f22275b8e9179
chiyaw <chiya@Shreyas-MacBook-Air.local>
2026-04-24 14:53:52 +0530

    cors setup

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5176"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
